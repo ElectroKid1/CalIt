1438a331beaffbd80116fec17a054eac5987dea9
ElectroKid1 <44017121+ElectroKid1@users.noreply.github.com>
2019-06-20 23:32:10 -0700

    added the functions of the op button
    used the grid() function on the next line of the entry box

--- calculator_code.py
+++ calculator_code.py
@@ -1,37 +1,43 @@
 from tkinter import *
 root=Tk()
 root.geometry("500x500")
-root.title("CalIt")
 
-#first input number
-text_data1=Label(root,text="Enter the first number",width=70).grid(row=0,column=0,columnspan=5)
-input_data1=Entry(root,width=70).grid(row=1,column=0,columnspan=5)
+def sum():
+    if e1.get()=="" or e2.get()=="":
+        w=Message(f1,text="This es error").grid(row=0,column=0)
+    else:
+        output=int(e1.get())+int(e2.get())
+        w=Message(f1,text="{}".format(output),height=40,width=80)
+        w.grid(row=0,column=0)
+l1=Label(root,text="Enter the 1st number:").grid(row=0,column=0,columnspan=4)
+e1=Entry(root,bd=5,width=80)
+e1.grid(row=1,column=0,columnspan=4)
+l2=Label(root,text="Enter the 2nd number:").grid(row=2,column=0,columnspan=4)
+e2=Entry(root,bd=5,width=80)
+e2.grid(row=3,column=0,columnspan=4)
+#use frame and insert message box in it.
+f1=Frame(root,height=40,width=480,highlightcolor="red",bg="blue")
+f1.grid(row=4,column=0,columnspan=5)
 
-#second input number
-text_data2=Label(root,text="Enter the second number",width=70).grid(row=2,column=0,columnspan=5)
-input_data2=Entry(root,width=70).grid(row=3,column=0,columnspan=5)
+#message box at row 4 
 
+b1=Button(root,text="1",width=16).grid(row=5,column=0)
+b2=Button(root,text="2",width=16).grid(row=5,column=1)
+b3=Button(root,text="3",width=16).grid(row=5,column=2)
+b_plus=Button(root,text="+",width=16,command=sum).grid(row=5,column=3)
 
-#Here will be the output message
-#row=4
-#numpad
-num1=Button(root,text="1",width=12).grid(row=5,column=0)
-num2=Button(root,text="2",width=12).grid(row=5,column=1)
-num3=Button(root,text="3",width=12).grid(row=5,column=2)
-num4=Button(root,text="4",width=12).grid(row=6,column=0)
-num5=Button(root,text="5",width=12).grid(row=6,column=1)
-num6=Button(root,text="6",width=12).grid(row=6,column=2)
-num7=Button(root,text="7",width=12).grid(row=7,column=0)
-num8=Button(root,text="8",width=12).grid(row=7,column=1)
-num9=Button(root,text="9",width=12).grid(row=7,column=2)
-num0=Button(root,text="0",width=26).grid(row=8,column=0,columnspan=2)
+b4=Button(root,text="4",width=16).grid(row=6,column=0)
+b5=Button(root,text="5",width=16).grid(row=6,column=1)
+b6=Button(root,text="6",width=16).grid(row=6,column=2)
+b_minus=Button(root,text="-",width=16).grid(row=6,column=3)
 
-op_add=Button(root,text="+",width=12,command=sum).grid(row=6,column=3)
-op_minus=Button(root,text="-",width=12).grid(row=5,column=3)
-op_div=Button(root,text="/",width=12).grid(row=7,column=3)
-op_mul=Button(root,text="X",width=12).grid(row=8,column=3)
-op_equals=Button(root,text="=",width=12,height=7).grid(row=5,column=4,rowspan=4)
-op_dot=Button(root,text=".",width=12).grid(row=8,column=2)
+b7=Button(root,text="7",width=16).grid(row=7,column=0)
+b8=Button(root,text="8",width=16).grid(row=7,column=1)
+b9=Button(root,text="9",width=16).grid(row=7,column=2)
+b_mul=Button(root,text="x",width=16).grid(row=7,column=3)
 
+b0=Button(root,text="0",width=34).grid(row=8,column=0,columnspan=2)
+b_dot=Button(root,text=".",width=16).grid(row=8,column=2)
+b_div=Button(root,text="/",width=16).grid(row=8,column=3)
 
 root.mainloop()
